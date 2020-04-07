4d60e01a4bb1eafccf90ece3359a3aa6ca78fead
radaelilucca <radaelidev@gmail.com>
2020-04-07 18:02:23 -0300

    packege json homepage

---
+++ build/precache-manifest.9b7856534e1f809b5c02ee0ae108a210.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6e70365cb14a07d2f1d1eb30f9af3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28a5142efefd4d1d6f17",
+    "url": "./static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "e02464de1f866b5015eb",
+    "url": "./static/css/main.395ff8d7.chunk.css"
+  },
+  {
+    "revision": "28a5142efefd4d1d6f17",
+    "url": "./static/js/2.0e7f51a6.chunk.js"
+  },
+  {
+    "revision": "492d7e05a4c2e81da7932422a4d31130",
+    "url": "./static/js/2.0e7f51a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02464de1f866b5015eb",
+    "url": "./static/js/main.b6467dc1.chunk.js"
+  },
+  {
+    "revision": "e736dabb8ee024a7b980",
+    "url": "./static/js/runtime-main.3993e964.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "./static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "./static/media/logo.7eea718b.svg"
+  }
+]);
